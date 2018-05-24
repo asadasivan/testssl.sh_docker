0fe59348507f81d3969f8da59c42b43f4566a420
Arunkumar Sadasivan <contact.arunsec@gmail.com>
2018-05-24 14:11:00 -0500

    code refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-arguments=""
-firsttime=true
-
-if [ ! -z "$FILEOUTPUT" ]; then # Not null
-   if [ $firsttime = true ]; then
-      arguments="$FILEOUTPUT"
-      firsttime=false  
-   else
-	  arguments="$arguments $FILEOUTPUT" 
-   fi	  	  
-    	  
-fi
-
-if [ ! -z $TESTS ]; then # Not null
-   if [ $firsttime = true ]; then
-      arguments="$TESTS"
-      firsttime=false  
-   else
-	  arguments="$arguments $TESTS" 
-   fi	  	  
-    	  
-fi
-
-exec /home/testssl.sh/testssl.sh $FILEOUTPUT $TESTS $URI
+exec /home/testssl.sh/testssl.sh "$@"
 exit 0
 
